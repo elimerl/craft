7dabe650336bad7136bfccd728e3ea44fb888908
elimerl <32183908+elimerl@users.noreply.github.com>
2021-03-06 04:10:37 +0000

    Deploying to gh-pages from  @ 9b6bcd7c38ccce226d19c6477d956a8abb37a0d0 🚀

--- dist/core/index.js
+++
@@ -1,8 +0,0 @@
-import {mcData, getRawIngredients} from "./util.js";
-import Fuse from "../../_snowpack/pkg/fusejs.js";
-export const fuse = new Fuse(Object.values(mcData.items), {
-  keys: ["displayName"]
-});
-export const search = (query) => {
-  return getRawIngredients(fuse.search(query)[0].item.id);
-};
